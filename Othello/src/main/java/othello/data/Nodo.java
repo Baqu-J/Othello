9f25c06e9fa6cdb53f2701521664a256976348eb
1215531 <franco.acevedo@est.fib.upc.edu>
2021-04-02 05:55:53 +0200

    NODOS_IA

---
+++ Othello/src/main/java/othello/data/Nodo.java
@@ -0,0 +1,55 @@
+
+package othello.data;
+
+import java.util.ArrayList;
+import othello.domain.Tablero;
+
+public class Nodo {
+
+    // ESTO PUEDE IR EN OTRO LUGAR
+    private static final int PESOS_HARD[][] = 
+        {{ 4,-3, 2, 2, 2, 2,-3, 4},
+         {-3,-4,-1,-1,-1,-1,-4,-3},
+         { 2,-1, 1, 0, 0, 1,-1, 2},
+         { 2,-1, 0, 1, 1, 0,-1, 2},
+         { 2,-1, 0, 1, 1, 0,-1, 2},
+         { 2,-1, 1, 0, 0, 1,-1, 2},
+         {-3,-4,-1,-1,-1,-1,-4,-3},
+         { 4,-3, 2, 2, 2, 2,-3, 4}};
+    
+    private int score;
+    private ArrayList<Nodo> hijos;
+    private Tablero tab;
+
+    public Nodo(ArrayList<Nodo> hijos, Tablero tab) {
+        this.hijos = hijos;
+        this.tab = tab;
+        this.score = calcularScore();
+    }
+  
+    
+    public static int calcularScore (/*CASILLA C*/){
+        
+        // AQUI VA EL CODIGO:
+        // -> sumar los pesos de las posiciones de las fichas de casilla c
+        //    restandole los pesos de las posiciones de las fichas del 
+        //    contrario usando la tabla PESOS. 
+        //    SUmando la cantidad de fichas c menos el contrario
+        // -> formula = SUM(pesos(c)) - SUM(pesos(contrario)) + #fichas(c) - #fichas(contrario)
+        
+        return 3;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    //OBTIENE LOS HIJOS PARTIENDO DEL TABLERO
+    public ArrayList<Nodo> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(ArrayList<Nodo> hijos) {
+        this.hijos = hijos;
+    }
+}
--- Othello/src/main/java/othello/domain/IA.java
+++ Othello/src/main/java/othello/domain/IA.java
@@ -2,10 +2,11 @@
 package othello.domain;
 import othello.data.Pair;
 import java.util.ArrayList;
+import othello.data.Nodo;
 
 enum dificultad{FACIL, NORMAL, DIFICIL};
 
-
+/**/
 public class IA extends GameState {
     
      private final dificultad opcion;
@@ -18,33 +19,60 @@ public class IA extends GameState {
          opcion = op;
      }
      
-    /**
-     *
-     * @return
-     */
     public dificultad getDificultad(){
-         return opcion;
+         return dificultad.FACIL;
     }
-    
+
+    /*
     private void funcionFacil(Tablero tab) {};
 
     private void funcionNormal(Tablero tab) {};
 
     private void funcionDificil(Tablero tab) { };
     
-   
-    public void realizar_jugada(Tablero tab){
+    
+    */
+    
+    private int alpha_beta(Nodo id, int profundidad, int alpha, int beta, boolean maximizing_player ){
+        if (profundidad == 0) return id.calcularScore();
+ 
+	if (maximizing_player){
+		int maxEval = -(int)Double.POSITIVE_INFINITY;
+                ArrayList<Nodo> hijos_aux1 = id.getHijos(); // corregir
+                for( Nodo d : hijos_aux1 ){
+                    int eval = alpha_beta(d,profundidad-1,alpha,beta,false);
+                    maxEval = Math.max(maxEval,eval);
+                    alpha = Math.max(alpha,eval);
+                    if(beta <= alpha) break;
+                }      
+		return maxEval;
+        }
+        else{
+		int minEval = (int)Double.POSITIVE_INFINITY;
+		ArrayList<Nodo> hijos_aux2 = id.getHijos(); // corregir
+                for( Nodo d : hijos_aux2 ){
+                    int eval = alpha_beta(d,profundidad-1,alpha,beta,true);
+                    minEval = Math.min(minEval,eval);
+                    alpha = Math.min(beta,eval);
+                    if(beta <= alpha) break;
+                }      
+                return minEval;
+        }
+    }
+    
+    public void realizar_jugada(Nodo raiz){
         switch(opcion) {
             case FACIL:
-                funcionFacil(tab);
+                /*alpha_beta();*/
             break;
 
             case NORMAL:
-                funcionNormal(tab);
+                /*alpha_beta();*/
             break;
 
             case DIFICIL:
-                funcionDificil(tab);
+                double inf = Double.POSITIVE_INFINITY;
+                int aux = alpha_beta(raiz,3,(int)-inf,(int)inf,true);
             break;
             
             default: break;
