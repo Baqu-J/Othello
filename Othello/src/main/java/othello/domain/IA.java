b8d88098f1ad3d0ed766a694a856e08ad5f74b9e
1215531 <franco.acevedo@est.fib.upc.edu>
2021-04-23 04:36:15 +0200

    heuristica_IA

--- Othello/src/main/java/othello/domain/IA.java
+++ Othello/src/main/java/othello/domain/IA.java
@@ -1,5 +1,6 @@
 
 package othello.domain;
+import java.util.ArrayList;
 import othello.data.Pair;
 import othello.data.Node;
 import othello.data.Tree;
@@ -9,7 +10,39 @@ enum dificultad{FACIL, NORMAL, DIFICIL};
 /**/
 public class IA extends GameState {
     
-     private dificultad opcion;
+    private static final int PESOS_DIFICIL[][]
+            = {{ 5,-3, 2, 2, 2, 2,-3, 5},
+               {-3,-4,-1,-1,-1,-1,-4,-3},
+               { 2,-1, 1, 0, 0, 1,-1, 2},
+               { 2,-1, 0, 1, 1, 0,-1, 2},
+               { 2,-1, 0, 1, 1, 0,-1, 2},
+               { 2,-1, 1, 0, 0, 1,-1, 2},
+               {-3,-4,-1,-1,-1,-1,-4,-3},
+               { 5,-3, 2, 2, 2, 2,-3, 5}};
+
+    private static final int PESOS_NORMAL[][]
+            = {{ 2, -2, 2, 2, 2, 2,-2, 2},
+               {-2, -4,-1,-1,-1,-1,-4,-2},
+               { 2, -1, 1, 0, 0, 1,-1, 2},
+               { 2, -1, 0, 1, 1, 0,-1, 2},
+               { 2, -1, 0, 1, 1, 0,-1, 2},
+               { 2, -1, 1, 0, 0, 1,-1, 2},
+               {-2, -4,-1,-1,-1,-1,-4,-2},
+               { 2, -2, 2, 2, 2, 2,-2, 2}};
+
+    private static final int PESOS_FACIL[][]
+            = {{-5, 3,-2,-2,-2,-2, 3,-5},
+               { 3, 4, 1, 1, 1, 1, 4, 3},
+               {-2, 1,-1, 0, 0,-1, 1,-2},
+               {-2, 1, 0,-1,-1, 0, 1,-2},
+               {-2, 1, 0,-1,-1, 0, 1,-2},
+               {-2, 1,-1, 0, 0,-1, 1,-2},
+               { 3, 4, 1, 1, 1, 1, 4, 3},
+               {-5, 3,-2,-2,-2,-2, 3,-5}};
+
+    
+    
+    private dificultad opcion;
 
     public void setOpcion(dificultad opcion) {
         this.opcion = opcion;
@@ -26,33 +59,40 @@ public class IA extends GameState {
     public dificultad getDificultad(){
          return dificultad.FACIL;
     }
-
-    /*private Pair alpha_beta(Node id, int profundidad, int alpha, int beta, boolean maximizing_player ){
-        if (profundidad == 0) return id.getCord();
- 
-	if (maximizing_player){
-		int maxEval = -(int)Double.POSITIVE_INFINITY;
-                ArrayList<Node> hijos_aux1 = id.getHijos(); // corregir
-                for( Node d : hijos_aux1 ){
-                    int eval = alpha_beta(d,profundidad-1,alpha,beta,false);
-                    maxEval = Math.max(maxEval,eval);
-                    alpha = Math.max(alpha,eval);
-                    if(beta <= alpha) break;
-                }      
-		return maxEval;
-        }
-        else{
-		int minEval = (int)Double.POSITIVE_INFINITY;
-		ArrayList<Node> hijos_aux2 = id.getHijos(); // corregir
-                for( Node d : hijos_aux2 ){
-                    int eval = alpha_beta(d,profundidad-1,alpha,beta,true);
-                    minEval = Math.min(minEval,eval);
-                    alpha = Math.min(beta,eval);
-                    if(beta <= alpha) break;
-                }      
-                return minEval;
-        }
-    }*/
+    
+    private int score_facil(ArrayList<Pair> A){
+        int score_aux = 0;
+        for(Pair d : A) 
+            score_aux += PESOS_FACIL[d.first()][d.second()] ;
+        return score_aux;
+    }
+    
+    private int score_normal(ArrayList<Pair> A){
+        int score_aux = 0;
+        for(Pair d : A) 
+            score_aux += PESOS_NORMAL[d.first()][d.second()] ;
+        return score_aux;
+    }
+    
+    //En la evaluacion del score DIFICIL se podria agregar la diferencia actual
+    //de fichas en el tablero
+    private int score_dificil(ArrayList<Pair> A){
+        int score_aux = 0;
+        for(Pair d : A) 
+            score_aux += PESOS_DIFICIL[d.first()][d.second()] ;
+        return score_aux;
+    }
+    
+    
+    //Recibo las posiciones de las fichas y calculo su heuristica
+    //en base a su dificultad
+    public int obtener_score(ArrayList<Pair> A, dificultad d){
+        int score = 0;
+        if(d == dificultad.FACIL) score = score_facil(A);
+        else if(d == dificultad.NORMAL) score = score_normal(A);
+        else score = score_dificil(A);
+        return score;
+    }
     
     Node alpha_beta(Tree<Node> t, int depth, int alpha, int beta, boolean maximizingPlayer) {
         if(depth == 0 || (t.getSubTrees()).isEmpty())
