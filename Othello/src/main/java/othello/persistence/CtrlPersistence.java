bbc6d09a00ae9d5be0432251542446c453a283e4
1215531 <franco.acevedo@est.fib.upc.edu>
2021-04-27 12:30:49 +0200

    actualitza_ctrlPersistencia

--- Othello/src/main/java/othello/persistence/CtrlPersistence.java
+++ Othello/src/main/java/othello/persistence/CtrlPersistence.java
@@ -64,49 +64,26 @@ public class CtrlPersistence {
     
     public int GuardarPartida(Partida p) { //borrar existente antes de guardar y check si guarda bien
         BorrarSavedGame(p);
-        serializador.createJSONfromPartida(p);
-        File file = new File("JSON/Escenarios/SavedGame.json");
-        if(file.exists()) return 1; // se guardó exitosamente
-        return 0; //no se guardó exitosamente
-        
-        /*
-        BorrarSavedGame(p);
         int ret = serializador.createJSONfromPartida(p);
         return ret;
-        */
     }
     
-    public void CargarPartida() { //check si existe NO DEBE BORRAR
-        /*
-        File file = new File("JSON/Partida/SavedGame.json");
-        if(file.exists()){
-       ---> Partida p = new Partida();
-            serializador.getPartidafromFile(p);
-            return p;
-        }   
+    public Partida CargarPartida() { //check si existe NO DEBE BORRAR
+        Partida p = null;
+        int ret = serializador.getPartidafromFile(p);
+        if(ret == 1) return p;
         return null;
-        */
     }
     
     public int GuardarEscenario(Escenario e) { //check si guarda bien
         int ret = serializador.createJSONfromEscenario(e);
-        File file = new File("JSON/Escenarios/" + e.getId() + ".json");
-        if(file.exists()) return 1;
         return ret;
-        
-        /*
-        int ret = serializador.createJSONfromEscenario(e);
-        return ret;
-        */
     }
     
     public Escenario CargarEscenario(String id) { //check si existe
-        File file = new File("JSON/Escenarios/"+ id +".json");
-        if(file.exists()){
-            Escenario e = new Escenario("id");
-            serializador.getEscenariofromFile(e, "JSON/Escenarios/"+id+".json");
-            return e;
-        }   
+        Escenario e = new Escenario("id");
+        int ret = serializador.getEscenariofromFile(e, "JSON/Escenarios/"+id+".json");
+        if(ret == 1) return e;
         return null;
     }
 }
