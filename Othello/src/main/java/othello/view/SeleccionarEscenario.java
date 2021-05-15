e9ae8b27bdbbb7b68edd4f0380c2210a138c7bd1
1215531 <franco.acevedo@est.fib.upc.edu>
2021-05-15 14:38:38 +0200

    ok frame Escenario

--- Othello/src/main/java/othello/view/SeleccionarEscenario.java
+++ Othello/src/main/java/othello/view/SeleccionarEscenario.java
@@ -98,6 +98,7 @@ public class SeleccionarEscenario extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
