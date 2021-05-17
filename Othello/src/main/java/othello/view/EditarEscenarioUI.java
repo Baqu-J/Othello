66e3580c8ccf034edcd759711280b6453c56abeb
jaume.baquero <jaume.baquero@est.fib.upc.edu>
2021-05-17 20:21:05 +0200

    editar Escenario reload grid

--- Othello/src/main/java/othello/view/EditarEscenarioUI.java
+++ Othello/src/main/java/othello/view/EditarEscenarioUI.java
@@ -33,7 +33,12 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
         this.setIconImage(image);
         //setVisible(true);
     }
-
+    
+    protected void reloadGrid(String[] grid) {
+        tableroUI1.clearGrid();
+        tableroUI1.fillGrid(grid);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -45,11 +50,9 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
         java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        panelSeleccionarEscenario1 = new othello.view.panelSeleccionarEscenario();
+        tableroUI1 = new othello.view.TableroUI();
         jPanel4 = new javax.swing.JPanel();
         btn_anterior = new javax.swing.JButton();
-        btn_seleccionar = new javax.swing.JButton();
         btn_siguiente = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
@@ -59,18 +62,12 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
         jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.PAGE_AXIS));
-
-        jPanel2.setLayout(new javax.swing.BoxLayout(jPanel2, javax.swing.BoxLayout.LINE_AXIS));
-
-        panelSeleccionarEscenario1.setPreferredSize(new java.awt.Dimension(600, 750));
-        jPanel2.add(panelSeleccionarEscenario1);
-
-        jPanel1.add(jPanel2);
+        jPanel1.add(tableroUI1);
 
         jPanel4.setLayout(new java.awt.GridBagLayout());
 
         btn_anterior.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_anterior.setText("Anterior");
+        btn_anterior.setText("Deshacer");
         btn_anterior.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_anteriorActionPerformed(evt);
@@ -84,23 +81,8 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
         gridBagConstraints.insets = new java.awt.Insets(7, 7, 7, 7);
         jPanel4.add(btn_anterior, gridBagConstraints);
 
-        btn_seleccionar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_seleccionar.setText("Seleccionar");
-        btn_seleccionar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_seleccionarActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridheight = java.awt.GridBagConstraints.RELATIVE;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.insets = new java.awt.Insets(7, 7, 7, 7);
-        jPanel4.add(btn_seleccionar, gridBagConstraints);
-
         btn_siguiente.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_siguiente.setText("Siguiente");
+        btn_siguiente.setText("Rehacer");
         btn_siguiente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_siguienteActionPerformed(evt);
@@ -142,24 +124,8 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
         
     }//GEN-LAST:event_btn_anteriorActionPerformed
 
-    private void btn_seleccionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_seleccionarActionPerformed
-        // TODO add your handling code here:
-        String name = panelSeleccionarEscenario1.jLabel1.getText();
-
-        iCtrlView.editEscenario(name);
-    }//GEN-LAST:event_btn_seleccionarActionPerformed
-
     private void btn_siguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_siguienteActionPerformed
-        if(!panelSeleccionarEscenario1.escenarios.isEmpty() && panelSeleccionarEscenario1.current < panelSeleccionarEscenario1.escenarios.size()-1) {
-            ++panelSeleccionarEscenario1.current;
-            String[] parts = panelSeleccionarEscenario1.escenarios.get(panelSeleccionarEscenario1.current).split(",");
-            panelSeleccionarEscenario1.jLabel1.setText(parts[0]);
-            String[] grid = new String[8*8];
-            for (int i = 1; i < parts.length; i++) {
-                grid[i-1] = parts[i];
-            }
-            panelSeleccionarEscenario1.reloadGrid(grid);
-        }
+        
     }//GEN-LAST:event_btn_siguienteActionPerformed
 
     private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
@@ -173,13 +139,11 @@ public class EditarEscenarioUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton backButton;
     private javax.swing.JButton btn_anterior;
-    private javax.swing.JButton btn_seleccionar;
     private javax.swing.JButton btn_siguiente;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private othello.view.panelSeleccionarEscenario panelSeleccionarEscenario1;
+    private othello.view.TableroUI tableroUI1;
     // End of variables declaration//GEN-END:variables
 }
